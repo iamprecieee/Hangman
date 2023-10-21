3ae225abf2ecdde874d7fce46cfaab17bc713a12
Sourcery AI <>
2023-10-21 05:17:25 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -15,48 +15,48 @@ if contract == "yes":
   difficulty = input("\nPLEASE CHOOSE YOUR PREFERRED DIFFICULTY LEVEL:\nEasy or Intermediate or Hard\n").lower()
   while difficulty not in words:
     difficulty = input("\nPLEASE CHOOSE YOUR PREFERRED DIFFICULTY LEVEL:\nEasy or Intermediate or Hard\n").lower
-  
+
   #randomly pick any word from the list of possible words depending on difficulty level
   random_word = random.choice(words[difficulty])
-    
+
   #create a blank list to match the random word letter for letter, such that the letters are replaced by "_"
-  blank = ["_" for x in random_word]
-  
+  blank = ["_" for _ in random_word]
+
   #set a looping condition using while and declare total number of lives
   status = False
   lives = 6
-  
+
   print(f"\nA word has been picked from the cookie jar.\n{' '.join(blank)}")
-  
+
   while not status:
     
     #ask the player to guess a letter from the random word
     guess = input("\nGuess a letter from this word:\n").lower()
     if guess in blank:
         print(f"\nYOU HAVE ALREADY GUESSED {guess}!")
-      
+
     #compare the random word with a guess and fill in the blank if it matches
     for x, letter in enumerate(random_word):
             if letter.lower() == guess:
                 blank[x] = letter
-    
+
     if guess in random_word.lower() and len(guess) == 1:
       print(f"\n{random.choice(exclam)}")
     print(f"\n{' '.join(blank)}")
-  
+
     #check nuumber of lives left and end game when there are no more lives
     if guess not in random_word.lower():
       print(f"\nTHE LETTER {guess} IS NOT IN THIS WORD. YOU LOSE A LIFE!")
-      lives -=1  
-      if lives == 1:
-        print("\nLAST CHANCE!\n")
+      lives -=1
       if lives == 0:
-          status = True
-          print("\nYOU LOSE!")
-       
+        status = True
+        print("\nYOU LOSE!")
+
+      elif lives == 1:
+        print("\nLAST CHANCE!\n")
     #display hangman status
     print(hangman[lives])  
-    
+
     #check if all letters have been field and end game if satisfied
     if "_" not in blank:
       status = True
